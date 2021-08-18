9efc1ec673b4ca39e2fb15c8ef60b5f369d1cb88
master-hit <46361656+master-hit@users.noreply.github.com>
2021-08-18 15:41:26 -0400

    Improved dropdown menu which turns dropdown button into a search bar.

--- functions.js
+++ functions.js
@@ -3,10 +3,16 @@ let x = 0;
 function showContent(){
     if (x === 0) {
         document.getElementById("dropdown-content").style.display= "block";
+        let e = document.getElementById("dropbtn");
+        e.className = "dropbtnClickedClass";
+        e.innerText = "Search: ";
         x = 1;
     }
     else {
         document.getElementById("dropdown-content").style.display= "none";
+        let e = document.getElementById("dropbtn");
+        e.className = "dropbtnClass";
+        e.innerText = "Genesis";
         x = 0;
     }
 
