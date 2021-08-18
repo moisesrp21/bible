46ef1638771942ef4c1b564091260dffcdd26081
master-hit <46361656+master-hit@users.noreply.github.com>
2021-08-18 17:05:09 -0400

    tix-1: Search Bar closes when user clicks elsewhere.

--- functions.js
+++ functions.js
@@ -1,18 +1,36 @@
-let x = 0;
+let x = 0; // 0 -> the search is off | 1 -> search is on
+
+document.addEventListener('click',checkDropdown);
+
+function checkDropdown(event) {
+    let target = event.target;
+    let e = document.getElementById("dropbtn");
+    if (!e.isSameNode(target)) {
+        document.getElementById("dropdown-content").style.display= "none";
+        e.className = "dropbtnClass";
+        e.type = "Button";
+        e.value = "Genesis";
+        x = 0;
+    }
+    console.log(target);
+}
 
 function showContent(){
     if (x === 0) {
         document.getElementById("dropdown-content").style.display= "block";
         let e = document.getElementById("dropbtn");
         e.className = "dropbtnClickedClass";
-        e.innerText = "Search: ";
+        e.type = "text";
+        e.value = "";
+        e.placeholder = "Search: ...";
         x = 1;
     }
     else {
         document.getElementById("dropdown-content").style.display= "none";
         let e = document.getElementById("dropbtn");
         e.className = "dropbtnClass";
-        e.innerText = "Genesis";
+        e.type = "Button";
+        e.value = "Genesis";
         x = 0;
     }
 
